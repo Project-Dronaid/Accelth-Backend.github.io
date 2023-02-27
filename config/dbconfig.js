e94ba68b99ca7cf4677682f54dbbff77bcefcdff
Prathmesh Sinha <77191932+PrathmeshRRDOMIN8@users.noreply.github.com>
2023-02-27 19:40:49 +0530

    first commit

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,4 +1,4 @@
 module.exports = {
     secret: 'accelthdronaidkey',
-    DATABASE_URL: 'mongodb+srv://accelthdronaid:accelthdronaid1234@cluster0.amk3uzi.mongodb.net/?retryWrites=true&w=majority'
+    database: 'mongodb+srv://accelthdronaid:accelthdronaid1234@cluster0.amk3uzi.mongodb.net/?retryWrites=true&w=majority'
 }

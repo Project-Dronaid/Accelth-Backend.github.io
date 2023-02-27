bca97050e04992a12625f7cd951d96581e28364e
Prathmesh Sinha <77191932+PrathmeshRRDOMIN8@users.noreply.github.com>
2023-02-27 17:13:43 +0530

    Commit 15

--- config/db.js
+++ config/db.js
@@ -5,20 +5,20 @@ const Grid = require('gridfs-stream')
 let gfs
 
 const connectDB = async () => {
-    try{
-        mongoose.connect(dbconfig.database,{
-            useNewUrlParser : true,
+    try {
+        mongoose.connect(dbconfig.DATABASE_URL, {
+            useNewUrlParser: true,
             useUnifiedTopology: true,
         })
         const con = mongoose.connection
-        con.on('open',()=> {
-            gfs = Grid(con.db,mongoose.mongo)
+        con.on('open', () => {
+            gfs = Grid(con.db, mongoose.mongo)
             gfs.collection('uploads')
             // console.log(gfs.collection('uploads').toString())
             console.log(`Mongo DB Connected`)
         })
     }
-    catch(error){
+    catch (error) {
         console.log(error)
         process.exit(1)
     }
